c32ffd8640c27f5961469614e9b4e271236622ac
Gabgasa <32471189+Gabgasa@users.noreply.github.com>
2019-10-22 11:01:24 -0300

    Commit inicial

---
+++ EmpresaLivros/EmpresaLivros/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpresaLivros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
