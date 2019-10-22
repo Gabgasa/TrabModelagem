554f0d2ef1cdfb3a1bbeb088906bbb4cd4b83557
Gabgasa <32471189+Gabgasa@users.noreply.github.com>
2019-10-22 14:00:26 -0300

    Inicio controladores

--- EmpresaLivros/EmpresaLivros/Controlador.cs
+++ EmpresaLivros/EmpresaLivros/Controlador.cs
@@ -6,10 +6,33 @@ namespace EmpresaLivros
 {
     class Controlador
     {
+        private ControladorPublicarLivro PublicarLivro = new ControladorPublicarLivro();
+        private ControladorListarLivrosTema ListarLivrosTema = new ControladorListarLivrosTema();
+        private ControladorSugestao Sugestao = new ControladorSugestao();
+        private ControladorPagamento Pagamento = new ControladorPagamento();
+
+
         static void Main(string[] args)
         {
-            Pedido p = new Pedido(1, 2);
-            Console.WriteLine($"{p.Codpedido}, {p.Valorpedido}");
+            Console.WriteLine("Bem vindo ao aplicativo de venda de Livros");
+            Console.WriteLine("Escolha uma das seguintes opções");
+            Console.WriteLine("\t1 - Publicar um Livro");
+            Console.WriteLine("\t2 - Buscar um livro baseado em um tema");
+            Console.WriteLine("\t3 - Comprar um livro");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Voce escolheu 1");
+                    
+                    break;
+                case "2":
+                    Console.WriteLine("Voce escolheu 2");
+                    break;
+                case "3":
+                    Console.WriteLine("Voce escolheu 3");
+                    break;
+            }
         }
     }
 }
---
+++ EmpresaLivros/EmpresaLivros/ControladorPublicarLivro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpresaLivros
+{
+    class ControladorPublicarLivro
+    {
+        public ControladorPublicarLivro()
+        {
+        }
+    }
+}
